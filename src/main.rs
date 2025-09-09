102e1e8f3f84cf219435f78224ab88a7494d33fa
Wealthometer <wealthhajoh87@gmail.com>
2025-09-09 17:28:23 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+use auth::{with_auth, Role};
+use error::Error::*;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::convert::Infallible;
+use std::sync::Arc;
+use warp::{reject, reply, Filter, Rejection, Reply};
+
+mod auth;
+mod error;
+
+#[derive(Clone)]
+pub struct User {
+    pub uid: String,
+    pub email: String,
+    pub pw: String,
+    pub role: String,
+}
+
+#[derive(Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub pw: String,
+}
+
+#[derive(Serialize)]
+pub struct LoginResponse {
+    pub token: String,
+}
+
+#[tokio::main]
+async fn main() {
+   let users = Arc::new(init_users());
+
+    let login_route = warp::path!("login")
+        .and(warp::post())
+        .and(with_users(users.clone()))
+        .and(warp::body::json())
+        .and_then(login_handler);
+
+    let user_route = warp::path!("user")
+        .and(with_auth(Role::User))
+        .and_then(user_handler);
+
+    let admin_route = warp::path!("admin")
+        .and(with_auth(Role::Admin))
+        .and_then(admin_handler);
+
+    let routes = login_route
+        .or(user_route)
+        .or(admin_route)
+        .recover()
+
 }
