3270ed143413471c789b36748cf4bd75f6c76ed3
Wealthometer <wealthhajoh87@gmail.com>
2025-09-22 22:13:47 +0100

    update

--- src/autth.rs
+++ src/autth.rs
@@ -1,10 +1,12 @@
-
-
+use chrono::prelude::*;
+use serde::{Deserialize, Serialize};
+use jsonwebtoken::{decode, encode, Algorithm}
 pub enum Role {
     User,
     Admin
 }
 
+#[derive(Clone, PartialEq)]
 impl Pole{
 pub fun from_str(role : &str) -> Role {
     match role {
@@ -14,11 +16,20 @@ pub fun from_str(role : &str) -> Role {
 }
 }
 
+#[derive(Debug, Deserialize, Serialize)]
 struct Claims {
-
+    sub: String,
+    role: String,
+    exp: usize,
 }
 
 pub fn create_jwt(uid: &str, role : &Role) -> Result<String> {
     let expiration = utc::now()
-        .checked_add_signed
+        .checked_add_signed(chrono::Duration::seconds(60))
+        .expect("valid timestamp")
+        .timestamp()
+
+    let claims = Claims{
+
+    }
 }
